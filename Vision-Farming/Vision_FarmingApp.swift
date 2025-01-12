f6255a1dbc6a01155ce0efe868e6f30a22b3c359
= <=>
2025-01-13 01:47:45 +0300

    Initial Commit

---
+++ Vision-Farming/Vision_FarmingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_FarmingApp.swift
+//  Vision-Farming
+//
+//  Created by Sinan Dinç on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Vision_FarmingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vision-FarmingTests/Vision_FarmingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_FarmingTests.swift
+//  Vision-FarmingTests
+//
+//  Created by Sinan Dinç on 1/13/25.
+//
+
+import Testing
+@testable import Vision_Farming
+
+struct Vision_FarmingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
